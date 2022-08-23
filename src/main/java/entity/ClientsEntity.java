a4ef897241a5c47f3ea6419efd5cb4cad5c03724
Michal Wlodarczyk <michalwlodarczykbusiness@gmail.com>
2022-08-23 18:15:49 +0200

    Initial commit

---
+++ src/main/java/entity/ClientsEntity.java
@@ -0,0 +1,64 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients", schema = "public", catalog = "ORO")
+public class ClientsEntity {
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "surname")
+    private String surname;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ClientsEntity that = (ClientsEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+}
---
+++ src/test/java/com/example/oro/OroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
